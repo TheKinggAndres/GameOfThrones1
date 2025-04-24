841c154f47bc7548710b096684fb39571dd92576
TheKinggAndres <puentes.andres.9090@gmail.com>
2025-04-24 12:03:05 +0000

    CambiosFinales

---
+++ js/informacion.js
@@ -0,0 +1,11 @@
+function mostrarInformacion() {
+    const app = document.getElementById("app");
+    app.innerHTML = "<h2>Información General</h2>";
+    app.innerHTML += `
+      <p>Bienvenido a la aplicacion sobre la API Game of Thrones.</p>
+      <p>Aquí podrás explorar los personajes, casas y más sobre el mundo de Westeros.</p>
+    `;
+
+    
+  }
+  
